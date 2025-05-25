51f4c465956d4520e02f7a3c899fd3dc0c622d25
ShardBlog <saulbuesosanz@gmail.com>
2025-05-25 18:22:50 +0200

    migrate to fastsearch

--- README.md
+++ README.md
@@ -35,7 +35,7 @@ Esto no es un buscador que te de resultados con IA y bla bla bla, es un buscador
 A ver, instalar jaja, me da mucha pereza hacer una extension de chrome o firefox para poner el buscador por defecto, asi que [te dejo aqui una extension de firefox](https://addons.mozilla.org/es-ES/firefox/addon/add-custom-search-engine/?utm_source=addons.mozilla.org&utm_medium=referral&utm_content=search) para personalizar tu buscador del navegador y ponerlo como buscador por defecto.
 Para ponerte el buscador utilizando la extension de firefox, debes rellenar estos datos :)
 **Name:** PicoSX
-**Search URL:** https://picosx.vercel.app/?q=%s
+**Search URL:** https://picosx.vercel.app/fastsearch?q=%s
 y ya ta :) el resto es seguir lo que te dice la extension y ya tendras PicoSX como buscador por defecto.
 
 Si usas chrome deja de vender todos tus datos a una empresa inglesa y instalate [**Zen Browser**](https://zen-browser.app/) (basado en Firefox) que merece la pena y no te roba hasta la sangre del cuerpo jaja salu2.
--- src/MainSearch.jsx
+++ src/MainSearch.jsx
@@ -1,106 +1,157 @@
 import bangs from "./bangs.js";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 export default function MainSearch() {
+  const [sxBarIcon, setSxBarIcon] = useState(
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="1.25"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className="icon icon-tabler icons-tabler-outline icon-tabler-search"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+      <path d="M21 21l-6 -6" />
+    </svg>
+  );
 
-    const [sxBarIcon, setSxBarIcon] = useState(
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-             stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"
-             className="icon icon-tabler icons-tabler-outline icon-tabler-search">
-            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-            <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"/>
-            <path d="M21 21l-6 -6"/>
-        </svg>
-    );
+  const [bangExtra, setBangExtra] = useState("");
 
-    const [bangExtra, setBangExtra] = useState("")
+  const [inputValue, setInputValue] = useState("");
 
-    const [inputValue, setInputValue] = useState("")
-
-    useEffect(() => {
-        let a = false;
-        document.querySelector(".bang-extra").style.left = document.querySelector(".search-bar input").offsetLeft + "px"
-        Object.keys(bangs).forEach(bang => {
-            if (inputValue.startsWith(bang)) {
-                a = true;
-                setBangExtra(bang);
-                setSxBarIcon(<span style={{display: "flex"}} dangerouslySetInnerHTML={{__html: bangs[bang].icon}} />)
-            }
-        })
-        if(!a) {
-            setBangExtra("");
-            setSxBarIcon(<span style={{display: "flex"}} dangerouslySetInnerHTML={{__html: bangs["<void>"].icon}} />)
-        }
-    }, [inputValue]);
-
-    function click(e) {
-        const button = document.querySelector(".search-btn");
-        const rippleContainer = button.querySelector(".ripple-container");
-        const ripple = document.createElement("div");
-        ripple.classList.add("ripple");
-        const rect = button.getBoundingClientRect();
-
-        ripple.style.top = (e.clientY - (rect.top)) + "px";
-        ripple.style.left = (e.clientX - (rect.left)) + "px";
-        rippleContainer.appendChild(ripple);
-        setTimeout(() => {
-            ripple.style.opacity = 0;
-            setTimeout(() => {
-                rippleContainer.removeChild(ripple);
-            }, 1000);
-        }, 1000);
+  useEffect(() => {
+    let a = false;
+    document.querySelector(".bang-extra").style.left =
+      document.querySelector(".search-bar input").offsetLeft + "px";
+    Object.keys(bangs).forEach((bang) => {
+      if (inputValue.startsWith(bang)) {
+        a = true;
+        setBangExtra(bang);
+        setSxBarIcon(
+          <span
+            style={{ display: "flex" }}
+            dangerouslySetInnerHTML={{ __html: bangs[bang].icon }}
+          />
+        );
+      }
+    });
+    if (!a) {
+      setBangExtra("");
+      setSxBarIcon(
+        <span
+          style={{ display: "flex" }}
+          dangerouslySetInnerHTML={{ __html: bangs["<void>"].icon }}
+        />
+      );
     }
+  }, [inputValue]);
 
-    function change(e) {
-        setInputValue(e.target.value)
-    }
+  function click(e) {
+    const button = document.querySelector(".search-btn");
+    const rippleContainer = button.querySelector(".ripple-container");
+    const ripple = document.createElement("div");
+    ripple.classList.add("ripple");
+    const rect = button.getBoundingClientRect();
 
-    return (
-        <div className="page">
-            <div className="full-center">
-                <h1>PICOSX</h1>
-                <form className="search-bar">
-                    {sxBarIcon}
-                    <span className="bang-extra">{bangExtra}</span>
-                    <input name="q" value={inputValue} type="text" placeholder="Busca aqui jaja" onChange={change} />
-                    <button className="search-btn" type="submit" onClick={click}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                             stroke="currentColor" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round"
-                             className="icon icon-tabler icons-tabler-outline icon-tabler-search">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                            <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"/>
-                            <path d="M21 21l-6 -6"/>
-                        </svg>
-                        <div className="ripple-container">
+    ripple.style.top = e.clientY - rect.top + "px";
+    ripple.style.left = e.clientX - rect.left + "px";
+    rippleContainer.appendChild(ripple);
+    setTimeout(() => {
+      ripple.style.opacity = 0;
+      setTimeout(() => {
+        rippleContainer.removeChild(ripple);
+      }, 1000);
+    }, 1000);
+  }
 
-                        </div>
-                    </button>
-                </form>
-            </div>
-            <div className="about-section">
-                <h1>Bangs<span>!</span></h1>
-                <div className="bang-container">
-                    {
-                        Object.keys(bangs).map(bang => {
-                            const bangData = bangs[bang];
-                            if(bang === "<void>") return;
-                            return <div className="bang-element" key={bang}>
-                                <span className="bang">{bang}</span>
-                                <span className="bang-mark">
-                                    <span className="icon" dangerouslySetInnerHTML={{__html: bangData.icon}} />
-                                    <span className="name">{bangData.name}</span>
-                                </span>
-                            </div>
-                        })
-                    }
-                </div>
-                <div>
-                    <p className="textp">Los bangs son prefijos de tu búsqueda que te permiten buscar rápidamente en otras páginas.</p>
-                    <p className="textp">El prefijo <b>!d</b> es un prefijo especial que te permitirá abrir un chat con el que hablar con Deepseek. No requiere inicio de sesion, es anónimo público gratuito y sin límite.</p>
-                    <p className="textp">El prefijo <b>!ch</b> es un prefijo especial que creará un nuevo chat directamente usando ChoniGPT (si tienes una cuenta claro).</p>
-                    <p className="textp">Tambien tenemos algunos shortcuts como <b>yt</b> para abrir youtube, entre otros. (Puedes verlos en nuestro <a href="https://github.com/pico190/picosx_">github</a>)</p>
-                </div>
-            </div>
+  function change(e) {
+    setInputValue(e.target.value);
+  }
+
+  return (
+    <div className="page">
+      <div className="full-center">
+        <h1>PICOSX</h1>
+        <form className="search-bar" action="fastsearch">
+          {sxBarIcon}
+          <span className="bang-extra">{bangExtra}</span>
+          <input
+            name="q"
+            value={inputValue}
+            type="text"
+            placeholder="Busca aqui jaja"
+            onChange={change}
+          />
+          <button className="search-btn" type="submit" onClick={click}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="icon icon-tabler icons-tabler-outline icon-tabler-search"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+              <path d="M21 21l-6 -6" />
+            </svg>
+            <div className="ripple-container"></div>
+          </button>
+        </form>
+      </div>
+      <div className="about-section">
+        <h1>
+          Bangs<span>!</span>
+        </h1>
+        <div className="bang-container">
+          {Object.keys(bangs).map((bang) => {
+            const bangData = bangs[bang];
+            if (bang === "<void>") return;
+            return (
+              <div className="bang-element" key={bang}>
+                <span className="bang">{bang}</span>
+                <span className="bang-mark">
+                  <span
+                    className="icon"
+                    dangerouslySetInnerHTML={{ __html: bangData.icon }}
+                  />
+                  <span className="name">{bangData.name}</span>
+                </span>
+              </div>
+            );
+          })}
         </div>
-    )
+        <div>
+          <p className="textp">
+            Los bangs son prefijos de tu búsqueda que te permiten buscar
+            rápidamente en otras páginas.
+          </p>
+          <p className="textp">
+            El prefijo <b>!d</b> es un prefijo especial que te permitirá abrir
+            un chat con el que hablar con Deepseek. No requiere inicio de
+            sesion, es anónimo público gratuito y sin límite.
+          </p>
+          <p className="textp">
+            El prefijo <b>!ch</b> es un prefijo especial que creará un nuevo
+            chat directamente usando ChoniGPT (si tienes una cuenta claro).
+          </p>
+          <p className="textp">
+            Tambien tenemos algunos shortcuts como <b>yt</b> para abrir youtube,
+            entre otros. (Puedes verlos en nuestro{" "}
+            <a href="https://github.com/pico190/picosx_">github</a>)
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
